6adc2cf9aac5753f2413ecb343407ace4abefcc3
chomnk <86968977+chomnk@users.noreply.github.com>
2024-01-21 05:21:14 -0500

    test

--- dinesmart/src/App.js
+++ dinesmart/src/App.js
@@ -10,7 +10,7 @@ function App() {
     if (isShopClicked) contentToRender = 
     <Shop 
         shopInfo={isShopClicked} 
-        username={"Teng"}
+        username={"teng"}
         shopToggle={setIsShopClicked}/>
 
     return (
