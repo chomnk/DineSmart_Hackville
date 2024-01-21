24b6436cd9871fad8fa8dc691edf985ee3c8de41
chomnk <86968977+chomnk@users.noreply.github.com>
2024-01-20 19:02:00 -0500

    r

--- dinesmart/src/Dashboard.js
+++ dinesmart/src/Dashboard.js
@@ -10,7 +10,7 @@ function Dashboard(props) {
 
     const fetchData = async () => {
         try {
-            const response = await fetch("localhost:3000/api/Restaurant");
+            const response = await fetch("http://localhost:3000/api/Restaurant");
             const result = await response.json();
             setShops(result);
         } catch (error) {
